e334c8f7c52ad20358d11cb73d9cd3720067c5bc
kobezzza <kobezzza@mail.ru>
2013-06-25 17:16:11 +0400

    Финальные исправления

--- demo/helloworld.node.js
+++ demo/helloworld.node.js
@@ -1,3 +1,2 @@
 var tpl = require('./helloworld.commonjs.ss.js').liveInit('../snakeskin.live.min.js');
-
 console.log(tpl.helloWorld());
--- index.js
+++ index.js
@@ -4,28 +4,31 @@
 
 var fs = require('fs'),
 	file, newFile,
-
-	commonjs;
+	commonJS;
 
 global.Snakeskin = require('./snakeskin');
 
-if (process.argv.length < 3) {
-	console.log('Usage: node compiler fname.ss');
+if (process.argv.length < 3 || process.argv[2] === '-h' || process.argv[2] === '--help') {
+	console.log('Usage: node compiler fname.ss [--commonjs | -cjs]');
+
 } else {
 	file = process.argv[2];
-	commonjs = process.argv[3] === '--commonjs';
-	newFile = process.argv[commonjs ? 4 : 3] || (file + '.js');
+	commonJS = process.argv[3] === '--commonjs' || process.argv[3] === '-cjs';
+	newFile = process.argv[commonJS ? 4 : 3] || (file + '.js');
 
 	fs.readFile(file, function (err, data) {
 		var res;
 
 		if (err) {
 			console.log(err);
+
 		} else {
-			res = Snakeskin.compile(String(data), commonjs, {file: file});
+			res = Snakeskin.compile(String(data), commonJS, {file: file});
+
 			fs.writeFile(newFile, res, function (err) {
 				if (err) {
 					console.log(err);
+
 				} else {
 					console.log('File "' + file + '" has been successfully compiled (' + newFile + ').');
 				}
