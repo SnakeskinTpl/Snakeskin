819ab59700a7258b0a03eb7db62eeb780774f8a0
Kobezzza <kobezzza@mail.ru>
2013-12-23 13:11:00 +0400

    Проверка типа закрываемых директив

--- snakeskin.js
+++ snakeskin.js
@@ -1970,20 +1970,44 @@ DirObj.prototype.prepareOutput = function (command, opt_sys, opt_isys, opt_break
 	return (!unEscape && !opt_sys ? 'Snakeskin.Filters.html(' : '') + res + (!unEscape && !opt_sys ? ')' : '');
 };
 var __NEJS_THIS__ = this;
+/**!
+ * @status stable
+ * @version 1.0.0
+ */
+
 /**
  * Директива end
  *
  * @param {string} command - название команды (или сама команда)
+ *
  * @param {number} commandLength - длина команды
  * @param {!DirObj} dirObj - объект управления директивами
- * @param {!Object} adv - дополнительные параметры
+ *
+ * @param {Object} adv - дополнительные параметры
+ * @param {!Object} adv.info - информация о шаблоне (название файлы, узла и т.д.)
  */
 Snakeskin.Directions['end'] = function (command, commandLength, dirObj, adv) {
 	var __NEJS_THIS__ = this;
 	dirObj.openBlockI--;
-	var args = arguments,
-		openBlockI = dirObj.openBlockI + 1,
-		res;
+
+	var args = arguments;
+	var openBlockI = dirObj.openBlockI + 1,
+		res = false;
+
+	// Если в директиве end указано название закрываемой директивы,
+	// то проверяем, чтобы оно совпадало с реально закрываемой директивой
+	function test(command, key) {
+		var __NEJS_THIS__ = this;
+		if (command !== 'end' && command !== key) {
+			throw dirObj.error('Invalid closing tag in the template, expected: ' +
+				key +
+				', declared: ' +
+				command +
+				', ' +
+				dirObj.genErrorAdvInfo(adv.info)
+			);
+		}
+	}
 
 	// Окончание шаблона
 	if (dirObj.openBlockI === 0) {
@@ -1992,13 +2016,14 @@ Snakeskin.Directions['end'] = function (command, commandLength, dirObj, adv) {
 	// Окончание простых блоков
 	} else if (dirObj.isNotSysPos(openBlockI)) {
 		Snakeskin.forEach(dirObj.posCache, function (el, key) {
-			var __NEJS_THIS__ = this;
+			
 			el = dirObj.getLastPos(key);
 
 			if (el && ((el.i !== void 0 && el.i === openBlockI) || el === openBlockI)) {
+				test(command, key);
 				res = true;
-				Snakeskin.Directions[key + 'End'].apply(Snakeskin, args);
 
+				Snakeskin.Directions[key + 'End'].apply(Snakeskin, args);
 				return false;
 			}
 
@@ -2012,10 +2037,11 @@ Snakeskin.Directions['end'] = function (command, commandLength, dirObj, adv) {
 
 	// Окончание системных блоков
 	Snakeskin.forEach(dirObj.sysPosCache, function (el, key) {
-		var __NEJS_THIS__ = this;
+		
 		el = dirObj.getLastPos(key);
 
 		if (el && ((el.i !== void 0 && el.i === openBlockI) || el === openBlockI)) {
+			test(command, key);
 			Snakeskin.Directions[key + 'End'].apply(Snakeskin, args);
 			return false;
 		}
