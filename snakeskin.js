638db7bae837aedc85c0551089e8614a0df61b0d
kobezzza <kobezzza@mail.ru>
2014-01-04 19:40:42 +0400

    Доработка

--- snakeskin.js
+++ snakeskin.js
@@ -3240,218 +3240,6 @@ var __NEJS_THIS__ = this;
  */
 
 Snakeskin.addDirective(
-	'forEach',
-
-	{
-		placement: 'template'
-	},
-
-	function (command) {
-		var __NEJS_THIS__ = this;
-		this.startDir();
-		if (this.isSimpleOutput()) {
-			var part = command.split('=>'),
-				val = this.prepareOutput(part[0], true);
-
-			var args = part[1] ? part[1].trim().split(',') : [];
-
-			var tmp = this.multiDeclVar('__TMP__ = ' + val),
-				cache = this.prepareOutput('__TMP__', true);
-
-			var oLength = '';
-			if (args.length >= 5) {
-				oLength +=
-					this.multiDeclVar('__TMP_LENGTH__ = 0') +
-					'for (' + this.multiDeclVar('key', false) + 'in ' + cache + ') {' +
-						'if (!' + cache + '.hasOwnProperty(' + this.prepareOutput('key', true) + ')) { continue; }' +
-						this.prepareOutput('__TMP_LENGTH__++;', true) +
-					'}';
-			}
-
-			var resStr =
-				tmp +
-				'if (' + cache + ') {' +
-					'if (Array.isArray(' + cache + ')) {' +
-						this.multiDeclVar('__TMP_LENGTH__ = __TMP__.length') +
-						'for (' + this.multiDeclVar('__I__ = 0') + this.prepareOutput('__I__ < __TMP_LENGTH__; __I__++', true) + ') {' +
-							(function () {
-								
-								var str = '';
-
-								for (var i = 0; i < args.length; i++) {
-									switch (i) {
-										case 0: {
-											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__I__]');
-										} break;
-
-										case 1: {
-											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
-										} break;
-
-										case 2: {
-											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
-										} break;
-
-										case 3: {
-											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
-										} break;
-
-										case 4: {
-											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
-										} break;
-									}
-								}
-
-								return str;
-							})()
-				;
-
-			var end =
-				'} else {' +
-					oLength +
-					this.multiDeclVar('__I__ = -1') +
-					'for (' + this.multiDeclVar('__KEY__', false) + 'in ' + cache + ') {' +
-						'if (!' + cache + '.hasOwnProperty(' + this.prepareOutput('__KEY__', true) + ')) { continue; }' +
-						this.prepareOutput('__I__++;', true) +
-
-						(function () {
-							
-							var str = '';
-
-							for (var i = 0; i < args.length; i++) {
-								switch (i) {
-									case 0: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__KEY__]');
-									} break;
-
-									case 1: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __KEY__');
-									} break;
-
-									case 2: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
-									} break;
-
-									case 3: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
-									} break;
-
-									case 4: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
-									} break;
-
-									case 5: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
-									} break;
-								}
-							}
-
-							return str;
-						})()
-				;
-
-			this.save(resStr);
-			this.structure.params = {
-				from: this.res.length,
-				end: end
-			};
-		}
-	},
-
-	function () {
-		var __NEJS_THIS__ = this;
-		if (this.isSimpleOutput()) {
-			var params = this.structure.params;
-			this.save('}' + params.end + this.res.substring(params.from) + '}}}');
-		}
-	}
-);
-
-Snakeskin.addDirective(
-	'forIn',
-
-	{
-		placement: 'template'
-	},
-
-	function (command) {
-		var __NEJS_THIS__ = this;
-		this.startDir();
-		if (this.isSimpleOutput()) {
-			var part = command.split('=>'),
-				val = this.prepareOutput(part[0], true);
-
-			var args = part[1] ? part[1].trim().split(',') : [];
-
-			var tmp = this.multiDeclVar('__TMP__ = ' + val),
-				cache = this.prepareOutput('__TMP__', true);
-
-			var oLength = '';
-			if (args.length >= 5) {
-				oLength +=
-					this.multiDeclVar('__TMP_LENGTH__ = 0') +
-					'for (' + this.multiDeclVar('key', false) + 'in ' + cache + ') {' +
-						this.prepareOutput('__TMP_LENGTH__++;', true) +
-					'}';
-			}
-
-			var resStr =
-				tmp +
-				'if (' + cache + ') {' +
-					oLength +
-					this.multiDeclVar('__I__ = -1') +
-					'for (' + this.multiDeclVar('__KEY__', false) + 'in ' + cache + ') {' +
-						this.prepareOutput('__I__++;', true) +
-
-						(function () {
-							
-							var str = '';
-
-							for (var i = 0; i < args.length; i++) {
-								switch (i) {
-									case 0: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__KEY__]');
-									} break;
-
-									case 1: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __KEY__');
-									} break;
-
-									case 2: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
-									} break;
-
-									case 3: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
-									} break;
-
-									case 4: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
-									} break;
-
-									case 5: {
-										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
-									} break;
-								}
-							}
-
-							return str;
-						})()
-			;
-
-			this.save(resStr);
-		}
-	},
-
-	function () {
-		var __NEJS_THIS__ = this;
-		if (this.isSimpleOutput()) {
-			this.save('}}');
-		}
-	}
-);
-
-Snakeskin.addDirective(
 	'for',
 
 	{
@@ -3477,9 +3265,9 @@ Snakeskin.addDirective(
 				(rgxp.test(parts[0]) ?
 					this.multiDeclVar(parts[0].replace(rgxp, '')) :
 					this.prepareOutput(parts[0], true)
-				) +
+					) +
 				this.prepareOutput(parts.slice(1).join(';'), true) +
-			') {');
+				') {');
 		}
 	}
 );
@@ -3628,7 +3416,257 @@ Snakeskin.addDirective(
 		}
 	}
 );
-var __NEJS_THIS__ = this;
+
+Snakeskin.addDirective(
+	'forEach',
+
+	{
+		placement: 'template'
+	},
+
+	function (command) {
+		var __NEJS_THIS__ = this;
+		if (!command) {
+			throw this.error('Invalid syntax');
+		}
+
+		this.startDir();
+		if (this.isSimpleOutput()) {
+			var part = command.split('=>'),
+				val = this.prepareOutput(part[0], true);
+
+			if (part.length > 2) {
+				throw this.error('Invalid syntax');
+			}
+
+			var args = part[1] ?
+				part[1].trim().split(',') : [];
+
+			var tmp = this.multiDeclVar('__TMP__ = ' + val),
+				cache = this.prepareOutput('__TMP__', true);
+
+			// Длина объекта
+			var oLength = '';
+			if (args.length >= 6) {
+				oLength +=
+					this.multiDeclVar('__TMP_LENGTH__ = 0') +
+					'for (' + this.multiDeclVar('__KEY__', false) + 'in ' + cache + ') {' +
+						'if (!' + cache + '.hasOwnProperty(' + this.prepareOutput('__KEY__', true) + ')) {' +
+							'continue;' +
+						'}' +
+
+						this.prepareOutput('__TMP_LENGTH__++;', true) +
+					'}';
+			}
+
+			// Для массивов
+			var resStr =
+				tmp +
+				'if (' + cache + ') {' +
+					'if (Array.isArray(' + cache + ')) {' +
+						this.multiDeclVar('__TMP_LENGTH__ = __TMP__.length') +
+						'for (' + this.multiDeclVar('__I__ = 0') + this.prepareOutput('__I__ < __TMP_LENGTH__; __I__++', true) + ') {' +
+							(function () {
+								
+								var str = '';
+
+								for (var i = 0; i < args.length; i++) {
+									switch (i) {
+										case 0: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__I__]');
+										} break;
+
+										case 1: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
+										} break;
+
+										case 2: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache);
+										} break;
+
+										case 3: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
+										} break;
+
+										case 4: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
+										} break;
+
+										case 5: {
+											str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
+										} break;
+									}
+								}
+
+								return str;
+							})()
+				;
+
+			// Для объектов
+			var end =
+				'} else {' +
+
+					oLength +
+					this.multiDeclVar('__I__ = -1') +
+
+					'for (' + this.multiDeclVar('__KEY__', false) + 'in ' + cache + ') {' +
+						'if (!' + cache + '.hasOwnProperty(' + this.prepareOutput('__KEY__', true) + ')) {' +
+							'continue;' +
+						'}' +
+
+						this.prepareOutput('__I__++;', true) +
+						(function () {
+							
+							var str = '';
+
+							for (var i = 0; i < args.length; i++) {
+								switch (i) {
+									case 0: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__KEY__]');
+									} break;
+
+									case 1: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __KEY__');
+									} break;
+
+									case 2: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache);
+									} break;
+
+									case 3: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
+									} break;
+
+									case 4: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
+									} break;
+
+									case 5: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
+									} break;
+
+									case 6: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
+									} break;
+								}
+							}
+
+							return str;
+						})()
+				;
+
+			this.save(resStr);
+			this.structure.params = {
+				from: this.res.length,
+				end: end
+			};
+		}
+	},
+
+	function () {
+		var __NEJS_THIS__ = this;
+		if (this.isSimpleOutput()) {
+			var params = this.structure.params;
+			this.save('}' + params.end + this.res.substring(params.from) + '}}}');
+		}
+	}
+);
+
+Snakeskin.addDirective(
+	'forIn',
+
+	{
+		placement: 'template'
+	},
+
+	function (command) {
+		var __NEJS_THIS__ = this;
+		if (!command) {
+			throw this.error('Invalid syntax');
+		}
+
+		this.startDir();
+		if (this.isSimpleOutput()) {
+			var part = command.split('=>'),
+				val = this.prepareOutput(part[0], true);
+
+			if (part.length > 2) {
+				throw this.error('Invalid syntax');
+			}
+
+			var args = part[1] ? part[1].trim().split(',') : [];
+
+			var tmp = this.multiDeclVar('__TMP__ = ' + val),
+				cache = this.prepareOutput('__TMP__', true);
+
+			var oLength = '';
+			if (args.length >= 6) {
+				oLength +=
+					this.multiDeclVar('__TMP_LENGTH__ = 0') +
+					'for (' + this.multiDeclVar('key', false) + 'in ' + cache + ') {' +
+						this.prepareOutput('__TMP_LENGTH__++;', true) +
+					'}';
+			}
+
+			var resStr =
+				tmp +
+				'if (' + cache + ') {' +
+					oLength +
+					this.multiDeclVar('__I__ = -1') +
+					'for (' + this.multiDeclVar('__KEY__', false) + 'in ' + cache + ') {' +
+						this.prepareOutput('__I__++;', true) +
+
+						(function () {
+							
+							var str = '';
+
+							for (var i = 0; i < args.length; i++) {
+								switch (i) {
+									case 0: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache + '[__KEY__]');
+									} break;
+
+									case 1: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __KEY__');
+									} break;
+
+									case 2: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = ' + cache);
+									} break;
+
+									case 3: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__');
+									} break;
+
+									case 4: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === 0');
+									} break;
+
+									case 5: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __I__ === __TMP_LENGTH__ - 1');
+									} break;
+
+									case 6: {
+										str += __NEJS_THIS__.multiDeclVar(args[i] + ' = __TMP_LENGTH__');
+									} break;
+								}
+							}
+
+							return str;
+						})()
+			;
+
+			this.save(resStr);
+		}
+	},
+
+	function () {
+		var __NEJS_THIS__ = this;
+		if (this.isSimpleOutput()) {
+			this.save('}}');
+		}
+	}
+);var __NEJS_THIS__ = this;
 /*!
  * @status stable
  * @version 1.0.0
