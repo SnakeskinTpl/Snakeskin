b6a7b655c55b856af4addf9553c97bffeb163486
kobezzza <kobezzza@mail.ru>
2014-01-04 19:47:14 +0400

    Доработка

--- snakeskin.js
+++ snakeskin.js
@@ -361,6 +361,10 @@ var sysConst = {
 	'__SNAKESKIN_RESULT__': true,
 	'__SNAKESKIN_CDATA__': true,
 	'__I_PROTO__': true,
+	'__I__': true,
+	'__TMP__': true,
+	'__TMP_LENGTH__': true,
+	'__KEY__': true,
 	'$_': true
 };
 
