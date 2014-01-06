9e5ae99ff9ea1a524a0bf6472561113b6a1c24cd
Kobezzza <kobezzza@mail.ru>
2014-01-06 19:25:35 +0400

    Новая директива try-catch-finally

--- snakeskin.js
+++ snakeskin.js
@@ -2817,8 +2817,6 @@ Snakeskin.addDirective(
 				this.source.substring(this.i - commandLength - 1);
 
 			this.initTemplateCache(tplName);
-			console.log(this.source);
-
 			this.startDir(this.structure.name);
 
 			this.i = this.startI - 1;
@@ -3858,6 +3856,66 @@ Snakeskin.addDirective(
 		}
 	}
 );var __NEJS_THIS__ = this;
+/*!
+ * @status stable
+ * @version 1.0.0
+ */
+
+Snakeskin.addDirective(
+	'try',
+
+	{
+		placement: 'template'
+	},
+
+	function () {
+		var __NEJS_THIS__ = this;
+		this.startDir();
+		if (this.isSimpleOutput()) {
+			this.save('try {');
+		}
+	}
+);
+
+Snakeskin.addDirective(
+	'catch',
+
+	{
+		placement: 'template',
+		notEmpty: true
+	},
+
+	function (command) {
+		var __NEJS_THIS__ = this;
+		if (this.structure.name !== 'try') {
+			throw this.error('Directive "' + this.name + '" can only be used with a "try"');
+		}
+
+		if (this.isSimpleOutput()) {
+			this.save('} catch (' + this.declVar(command) + ') {');
+		}
+	}
+);
+
+Snakeskin.addDirective(
+	'finally',
+
+	{
+		placement: 'template'
+	},
+
+	function () {
+		var __NEJS_THIS__ = this;
+		if (this.structure.name !== 'try') {
+			throw this.error('Directive "' + this.name + '" can only be used with a "try"');
+		}
+
+		if (this.isSimpleOutput()) {
+			this.save('} finally {');
+		}
+	}
+);
+var __NEJS_THIS__ = this;
 /**!
  * @status stable
  * @version 1.0.0
