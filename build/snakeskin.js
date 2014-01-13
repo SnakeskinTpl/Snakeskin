3d4374059f1947e1b3493088723a6d85522dd799
Kobezzza <kobezzza@mail.ru>
2014-01-13 18:09:43 +0400

    Доработка

--- build/snakeskin.js
+++ build/snakeskin.js
@@ -576,6 +576,7 @@ function DirObj(src, params) {
 
 			'exports.init = function (obj) { ' +
 				'Snakeskin = typeof obj === "object" ? obj : require(obj);' +
+				'delete exports.init;' +
 				'exec();' +
 				'return this;' +
 			'};' +
@@ -4615,40 +4616,3 @@ Snakeskin.addDirective(
 	}
 
 })(typeof window === 'undefined');
-var __NEJS_THIS__ = this;
-/**!
- * @status stable
- * @version 1.0.0
- */
-
-if (typeof window === 'undefined') {
-	var fs = require('fs');
-	var path = require('path');
-	var jossy = require('jossy');
-
-	Snakeskin.compileFile = function (file, params) {
-		var __NEJS_THIS__ = this;
-		var save = params.output || file + '.js',
-			commonJS = typeof params.commonJS === 'undefined' ? true : params.commonJS;
-
-		function action(data) {
-			var __NEJS_THIS__ = this;
-			fs.writeFileSync(save, Snakeskin.compile(String(data), commonJS, {file: file}));
-		}
-
-		if (params.build) {
-			jossy.compile(file, null, null, function (err, data) {
-				
-				if (err) {
-					console.log(err);
-
-				} else {
-					action(data);
-				}
-			});
-
-		} else {
-			action(fs.readFileSync(file));
-		}
-	};
-}
