c345bf4b9e627bd8e3e713c07410d0dcc15b3f91
Kobezzza <kobezzza@mail.ru>
2014-01-15 14:43:59 +0400

    Доработка консольного API && обновление доки

--- index.js
+++ index.js
@@ -3,32 +3,44 @@
 var __NEJS_THIS__ = this;
 /**!
  * @status stable
- * @version 1.0.0
+ * @version 1.1.0
  */
 
 global.Snakeskin = require('./snakeskin');
-var prog = require('commander');
+var Program = require('commander');
 
-prog
+Program
 	.version(Snakeskin.VERSION.join('.'))
 	.option('-s, --source [src]', 'source file')
 	.option('-o, --output [src]', 'output file')
 	.option('-n, --commonjs', 'compile templates as commonJS module')
 	.parse(process.argv);
 
+var input;
+
+if (!Program.source) {
+	input = process.argv[2];
+}
+
 var fs = require('fs');
 var jossy = require('jossy');
 
-var file = prog.source,
-	newFile = prog.output || (file + '.js');
+var file = Program.source,
+	newFile = Program.output || (file + '.js');
 
-jossy.compile(file, null, null, function (err, data) {
-	
-	if (err) {
-		console.log(err);
+function action(data) {
+	var __NEJS_THIS__ = this;
+	if (!data) {
+		Program.help();
+	}
+
+	var str = String(data);
+
+	if (input && !Program.output) {
+		console.log(Snakeskin.compile(str, Program.commonjs, {file: file}));
 
 	} else {
-		fs.writeFile(newFile, Snakeskin.compile(String(data), prog.commonjs, {file: file}), function (err) {
+		fs.writeFile(newFile, Snakeskin.compile(str, Program.commonjs, {file: file}), function (err) {
 			
 			if (err) {
 				console.log(err);
@@ -38,4 +50,20 @@ jossy.compile(file, null, null, function (err, data) {
 			}
 		});
 	}
-});
+}
+
+if (file) {
+	jossy.compile(file, null, null, function (err, data) {
+		
+		if (err) {
+			console.log(err);
+
+		} else {
+			action(data);
+		}
+	});
+
+} else {
+	action(input);
+}
+
