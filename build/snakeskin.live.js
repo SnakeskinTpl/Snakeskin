811b987fa9a76ffe865aaf10964ad73cb73be8c9
kobezzza <kobezzza@mail.ru>
2014-08-11 12:11:03 +0400

    Обновление версии

--- build/snakeskin.live.js
+++ build/snakeskin.live.js
@@ -1,11 +1,11 @@
 /*!
- * Snakeskin v4.0.0 (live)
+ * Snakeskin v4.0.1 (live)
  * https://github.com/kobezzza/Snakeskin
  *
  * Released under the MIT license
  * https://github.com/kobezzza/Snakeskin/blob/master/LICENSE
  *
- * Date: Mon, 11 Aug 2014 08:07:22 GMT
+ * Date: Mon, 11 Aug 2014 08:10:43 GMT
  */
 
 Array.isArray = Array.isArray || function (obj) {
@@ -27,7 +27,7 @@ var Snakeskin = {
 	 * @expose
 	 * @type {!Array}
 	 */
-	VERSION: [4, 0, 0],
+	VERSION: [4, 0, 1],
 
 	/**
 	 * Пространство имён для директив
