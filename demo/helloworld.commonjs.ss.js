b1a794d7ab0dce1980f61cd2dbfedcf5ac58d4ea
kobezzza <kobezzza@mail.ru>
2014-08-14 18:01:03 +0400

    Обновленеи примеров

--- demo/helloworld.commonjs.ss.js
+++ demo/helloworld.commonjs.ss.js
@@ -1,4 +1,4 @@
-/* Snakeskin v4.0.0, label <1407242422187>, generated at <1407242617683> Tue Aug 05 2014 16:43:37 GMT+0400 (Московское время (зима)). This code is generated automatically, don't alter it. */
+/* Snakeskin v4.0.9, label <1407503300938>, generated at <1408024780496> Thu Aug 14 2014 17:59:40 GMT+0400 (Московское время (зима)). This code is generated automatically, don't alter it. */
 (function() {
     var Snakeskin = global.Snakeskin;
     exports['init'] = function(obj) {
@@ -19,7 +19,7 @@
             __VARS__ = Snakeskin.Vars,
             __LOCAL__ = Snakeskin.LocalVars,
             __STR__, __TMP__, __J__;
-        var $_ = __LOCAL__['$_88a1a']; /* Snakeskin template: helloWorld; name  */
+        var $_ = __LOCAL__['$_7d9ca']; /* Snakeskin template: helloWorld; name  */
         this.helloWorld = function(name) {
             var __THIS__ = this,
                 callee = __ROOT__.helloWorld;
@@ -29,13 +29,16 @@
             }
             var __RESULT__ = '',
                 $_;
+            var getTplResult = function() {
+                return __RESULT__;
+            };
             var __RETURN__ = false,
                 __RETURN_VAL__;
             var TPL_NAME = 'helloWorld',
                 PARENT_TPL_NAME;
             name = arguments[0] = name != null ? name : 'world';
             __RESULT__ += '<h1>Hello ';
-            __RESULT__ += __FILTERS__.html(__FILTERS__.undef(name));
+            __RESULT__ += __FILTERS__.html(__FILTERS__.undef(name), false);
             __RESULT__ += '!</h1> ';
             return __RESULT__;
         };
