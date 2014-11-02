570392c551d1cba6a2ab1aeff33231f822f2b8bf
Kobezzza <kobezzza@mail.ru>
2014-11-02 13:02:24 +0400

    Перевод в es5

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-var path = require('path');
-var fs = require('fs'),
-	exists = fs.existsSync || path.existsSync;
+var path = require('path'),
+	fs = require('fs');
 
-var assert = require('assert');
-var snakeskin = require('./snakeskin');
-var testFolder = path.resolve(__dirname, 'tests');
+var assert = require('assert'),
+	snakeskin = require('./snakeskin');
+
+var testFolder = path.resolve(__dirname, 'test');
 
 global.i18n = function (str) {
 	return str;
@@ -27,19 +27,19 @@ function run(params) {
 		debug = params.debug = {};
 
 	prfx++;
-	fs.readdirSync(testFolder).forEach((file) => {
+	fs.readdirSync(testFolder).forEach(function (file) {
 		if (path.extname(file) === '.ss') {
-			let src = path.join(testFolder, file),
+			var src = path.join(testFolder, file),
 				txt = String(fs.readFileSync(src)).split('###');
 
-			txt.forEach((el, i) => {
+			txt.forEach(function (el, i) {
 				txt[i] = el.trim();
 			});
 
-			let starts = txt[0].split(/[\r\n]+/),
+			var starts = txt[0].split(/[\r\n]+/),
 				results = txt[2].split('***');
 
-			let obj = {
+			var obj = {
 				tpl: txt[1],
 				id: path.basename(file, '.ss'),
 				js: []
@@ -50,33 +50,33 @@ function run(params) {
 			}
 
 			try {
-				let start = Date.now();
-				let res = snakeskin.compile(txt[1], params, {
+				var start = Date.now();
+				var res = snakeskin.compile(txt[1], params, {
 					file: path.join(testFolder, file)
 				});
 
 				if (!prfx) {
-					console.log(`${file} ${Date.now() - start}ms`);
+					console.log(file + ' ' + Date.now() - start + 'ms');
 				}
 
-				fs.writeFileSync(`${src}_${prfx}.js`, res);
+				fs.writeFileSync(src + '_' + prfx + '.js', res);
 
 			} catch (err) {
-				fs.writeFileSync(errorPath, `File: ${file}\n\n${err.message}${debug['code'] ? `\n\nCode:\n\n${debug['code']}` : ''}`);
+				fs.writeFileSync(errorPath, 'File: ' + file + '\n\n' + err.message + (debug['code'] ? '\n\nCode:\n\n' + debug['code'] : ''));
 				throw err;
 			}
 
-			let tpl = require(`./tests/${file}_${prfx}.js`).init(snakeskin);
+			var tpl = require('./tests/' + file + '_' + prfx + '.js').init(snakeskin);
 
-			starts.forEach((el, i) => {
-				let params = el.split(' ; '),
+			starts.forEach(function (el, i) {
+				var params = el.split(' ; '),
 					res = '';
 
 				try {
-					obj.js.push(`equal(${params[0]}(${params.slice(1)}).trim(), '${results[i].trim()}');`);
+					obj.js.push('equal(' + params[0] + '(' + params.slice(1) + ').trim(), "' + results[i].trim() + '");');
 
 					// eval нужен чтобы сохранить информацию о типах
-					res = eval(`tpl.${params[0]}(${params.slice(1)})`);
+					res = eval('tpl.' + params[0] + '(' + params.slice(1) + ')');
 					res = res != null ? res.trim() : '';
 
 					assert.equal(
@@ -85,11 +85,11 @@ function run(params) {
 					);
 
 				} catch (err) {
-					console.error(`File: ${file} - ${prfx} (${options}), Tpl: ${params[0]}`);
+					console.error('File: ' + file + ' - ' + prfx + ' ' + '(' + options + '), Tpl: ' + params[0]);
 
 					fs.writeFileSync(
 						errorPath,
-						`File: ${file} - ${prfx} (${options}), Tpl: ${params[0]}\n\nResult:\n${res}\n\nExpected:\n${results[i].trim()}\n\nTest:\n${txt[1]}\n\nCode:\n${debug['code']}`
+						'File: ' + file + ' - ' + prfx + ' ' + '(' + options + '), Tpl: ' + params[0] + '\n\nResult:\n' + res + '\n\nExpected:\n' + results[i].trim() + '\n\nTest:\n' + txt[1] + '\n\nCode:\n' + debug['code']
 					);
 
 					throw err;
@@ -140,6 +140,6 @@ run({
 
 fs.writeFileSync(path.join(__dirname, 'tests', 'tests.html'), tpls.test(asserts));
 
-if (exists(errorPath)) {
+if (fs.existsSync(errorPath)) {
 	fs.unlinkSync(errorPath);
 }
