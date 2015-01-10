ada900ee005d21ed60a6ab2df99a2a6782057718
kobezzza <kobezzza@gmail.com>
2015-01-10 13:02:38 +0300

    Рефакторинг

--- predefs/src/index.js
+++ predefs/src/index.js
@@ -8,3 +8,82 @@
 
 //#include ./standart/*.js
 //#include ../../externs.js
+
+/**
+ * @interface
+ * @param {string} src
+ * @param {!Object} params
+ * @param {?function(!Error)=} [params.onError]
+ * @param {boolean} params.useStrict
+ * @param {boolean} params.throws
+ * @param {string} params.exports
+ * @param {boolean} params.inlineIterators
+ * @param {boolean} params.autoReplace
+ * @param {Object=} [params.macros]
+ * @param {?string=} [params.renderAs]
+ * @param {string|boolean} [params.doctype]
+ * @param {boolean} params.localization
+ * @param {string} params.i18nFn
+ * @param {Object=} [params.language]
+ * @param {string} params.lineSeparator
+ * @param {boolean} params.tolerateWhitespace
+ * @param {boolean} params.replaceUndef
+ * @param {boolean} params.escapeOutput
+ * @param {string} params.bemFilter
+ * @param {string} params.renderMode
+ * @param {Array=} [params.lines]
+ * @param {DirObj=} [params.parent]
+ * @param {?boolean=} [params.needPrfx]
+ * @param {RegExp=} [params.ignore]
+ * @param {Array=} [params.scope]
+ * @param {Object=} [params.vars]
+ * @param {Array=} [params.consts]
+ * @param {Object=} [params.proto]
+ * @param {Object=} [params.info]
+ */
+function $$SnakeskinDirObj(src, params) {}
+
+/** @type {string} */
+$$SnakeskinDirObj.prototype.renderMode;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.tolerateWhitespace;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.inlineIterators;
+
+/** @type {(string|boolean)} */
+$$SnakeskinDirObj.prototype.doctype;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.replaceUndef;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.escapeOutput;
+
+/** @type {(?string|undefined)} */
+$$SnakeskinDirObj.prototype.renderAs;
+
+/** @type {string} */
+$$SnakeskinDirObj.prototype.exports;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.autoReplace;
+
+/** @type {(Object|undefined)} */
+$$SnakeskinDirObj.prototype.macros;
+
+/** @type {string} */
+$$SnakeskinDirObj.prototype.bemFilter;
+
+/** @type {boolean} */
+$$SnakeskinDirObj.prototype.localization;
+
+/** @type {string} */
+$$SnakeskinDirObj.prototype.i18nFn;
+
+/** @type {(Object|undefined)} */
+$$SnakeskinDirObj.prototype.language;
+
+/** @type {(RegExp|undefined)} */
+$$SnakeskinDirObj.prototype.ignore;
