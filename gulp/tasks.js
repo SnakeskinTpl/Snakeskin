cb1151f90014840174e638fc8a07c43aac260aae
kobezzza <kobezzza@gmail.com>
2015-12-08 18:19:28 +0300

    :wrench:

--- gulp/tasks.js
+++ gulp/tasks.js
@@ -21,7 +21,38 @@ const
 	bump = require('gulp-bump'),
 	run = require('gulp-run');
 
-exports.head = (cb) => {
+gulp.task('copyright', (cb) => {
+	gulp.src('./LICENSE')
+		.pipe(replace(/(Copyright \(c\) )(\d+)-?(\d*)/, (sstr, intro, from, to) => {
+			const year = new Date().getFullYear();
+			return intro + from + (to || from != year ? '-' + year : '');
+		}))
+
+		.pipe(gulp.dest('./'))
+		.on('end', cb);
+});
+
+gulp.task('bump', (cb) => {
+	gulp.src('./*.json')
+		.pipe(bump({version: helpers.getVersion()}))
+		.pipe(gulp.dest('./'))
+		.on('end', cb);
+});
+
+gulp.task('npmignore', (cb) => {
+	gulp.src('./.npmignore')
+		.pipe(replace(/([\s\S]*?)(?=# NPM ignore list)/, fs.readFileSync('./.gitignore') + '\n'))
+		.pipe(gulp.dest('./'))
+		.on('end', cb);
+});
+
+gulp.task('yaspeller', (cb) => {
+	run('yaspeller ./').exec()
+		.on('error', helpers.error(cb))
+		.on('finish', cb);
+});
+
+gulp.task('head', (cb) => {
 	global.readyToWatcher = false;
 
 	const
@@ -60,35 +91,4 @@ exports.head = (cb) => {
 		global.readyToWatcher = true;
 		cb();
 	});
-};
-
-exports.copyright = (cb) => {
-	gulp.src('./LICENSE')
-		.pipe(replace(/(Copyright \(c\) )(\d+)-?(\d*)/, (sstr, intro, from, to) => {
-			const year = new Date().getFullYear();
-			return intro + from + (to || from != year ? '-' + year : '');
-		}))
-
-		.pipe(gulp.dest('./'))
-		.on('end', cb);
-};
-
-exports.bump = (cb) => {
-	gulp.src('./*.json')
-		.pipe(bump({version: helpers.getVersion()}))
-		.pipe(gulp.dest('./'))
-		.on('end', cb);
-};
-
-gulp.task('npmignore', (cb) => {
-	gulp.src('./.npmignore')
-		.pipe(replace(/([\s\S]*?)(?=# NPM ignore list)/, fs.readFileSync('./.gitignore') + '\n'))
-		.pipe(gulp.dest('./'))
-		.on('end', cb);
 });
-
-exports.yaspeller = (cb) => {
-	run('yaspeller ./').exec()
-		.on('error', helpers.error(cb))
-		.on('finish', cb);
-};
