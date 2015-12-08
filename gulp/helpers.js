7b55a3ff4b080c90929ddec342ecfaf2985b9541
kobezzza <kobezzza@gmail.com>
2015-12-08 18:13:59 +0300

    :art: Refactoring

--- gulp/helpers.js
+++ gulp/helpers.js
@@ -12,28 +12,33 @@ const
 	path = require('path'),
 	fs = require('fs');
 
-exports.getVersion = () => {
-	const file = fs.readFileSync(path.join(__dirname, '../lib/core.js'));
-	return /VERSION\s*(?::|=)\s*\[(\d+,\s*\d+,\s*\d+)]/.exec(file)[1]
-		.split(/\s*,\s*/)
-		.join('.');
+module.exports = {
+	getBuilds() {
+		delete require.cache[require.resolve('../builds')];
+		return Object(require('../builds'));
+	},
+
+	getVersion() {
+		const file = fs.readFileSync(path.join(__dirname, '../lib/core.js'));
+		return /VERSION\s*(?::|=)\s*\[(\d+,\s*\d+,\s*\d+)]/.exec(file)[1]
+			.split(/\s*,\s*/)
+			.join('.');
+	},
+
+	getHead(opt_version, opt_key) {
+		return '' +
+			'/*!\n' +
+			` * Snakeskin${opt_version ? ` v${this.getVersion()}` : ''}${opt_key ? ` (${opt_key})` : ''}\n` +
+			' * https://github.com/SnakeskinTpl/Snakeskin\n' +
+			' *\n' +
+			' * Released under the MIT license\n' +
+			' * https://github.com/SnakeskinTpl/Snakeskin/blob/master/LICENSE\n';
+	},
+
+	error(cb) {
+		return (err) => {
+			console.error(err.message);
+			cb();
+		};
+	}
 };
-
-exports.getHead = (opt_version, opt_key) =>
-	'/*!\n' +
-	` * Snakeskin${opt_version ? ` v${this.getVersion()}` : ''}${opt_key ? ` (${opt_key})` : ''}\n` +
-	' * https://github.com/SnakeskinTpl/Snakeskin\n' +
-	' *\n' +
-	' * Released under the MIT license\n' +
-	' * https://github.com/SnakeskinTpl/Snakeskin/blob/master/LICENSE\n';
-
-exports.getBuilds = () => {
-	delete require.cache[require.resolve('../builds')];
-	return Object(require('../builds'));
-};
-
-exports.error = (cb) =>
-	(err) => {
-		console.error(err.message);
-		cb();
-	};
