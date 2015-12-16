4d32a23e010ab483e8978ab22f11d4161acf03c9
kobezzza <kobezzza@gmail.com>
2015-12-16 16:38:56 +0300

    :art: Refactoring

--- src/helpers/cache.js
+++ src/helpers/cache.js
@@ -18,52 +18,47 @@ import { escapeEOLs } from './escape';
 /**
  * Returns data from the cache by the specified key
  *
- * @param {?string} cacheKey - cache key
+ * @param {?string} key - cache key
  * @param {string} code - source SS code
  * @param {!Object} params - compile parameters
  * @param {!Object} ctx - context
  * @return {(string|undefined)}
  */
-export function getFromCache(cacheKey, code, params, ctx) {
-	if (IS_NODE && ctx !== NULL && $globalFnCache[cacheKey]) {
-		$C($globalFnCache[cacheKey][code]).forEach((el, key) => {
+export function getFromCache(key, code, params, ctx) {
+	if (IS_NODE && ctx !== NULL && $globalFnCache[key]) {
+		$C($globalFnCache[key][code]).forEach((el, key) => {
 			ctx[key] = el;
 		});
 	}
 
 	const
-		cache = $globalCache[cacheKey] && $globalCache[cacheKey][code];
+		cache = $globalCache[key] && $globalCache[key][code];
 
-	if (cache) {
-		let
-			skip = false;
-
-		if (params.words) {
-			if (!cache.words) {
-				skip = true;
-
-			} else {
-				$C(cache.words).forEach((el, key) => {
-					params.words[key] = el;
-				});
-			}
-		}
-
-		if (params.debug) {
-			if (!cache.debug) {
-				skip = true;
-
-			} else {
-				$C(cache.debug).forEach((el, key) => {
-					params.debug[key] = el;
-				});
-			}
-		}
-
-		if (!skip) {
-			return cache.text;
-		}
+	if (!cache) {
+		return;
 	}
+
+	if (params.words) {
+		if (!cache.words) {
+			return;
+		}
+
+		$C(cache.words).forEach((el, key) => {
+			params.words[key] = el;
+		});
+	}
+
+	if (params.debug) {
+		if (!cache.debug) {
+			return;
+		}
+
+		$C(cache.debug).forEach((el, key) => {
+			params.debug[key] = el;
+		});
+	}
+
+	return cache.text;
 }
 
 /**
@@ -99,37 +94,40 @@ export function getCacheKey(params, ctx) {
 /**
  * Saves compiled template functions in the cache by the specified key
  *
- * @param {?string} cacheKey - cache key
+ * @param {?string} key - cache key
  * @param {string} code - source SS code
  * @param {!Object} params - compile parameters
  * @param {!Object} ctx - context
  */
-export function saveIntoFnCache(cacheKey, code, params, ctx) {
-	if (ctx !== NULL) {
-		ctx['init'](Snakeskin);
-
-		if (cacheKey && (params.cache || $globalFnCache[cacheKey])) {
-			$globalFnCache[cacheKey] = $globalFnCache[cacheKey] || {};
-			$globalFnCache[cacheKey][code] = ctx;
-		}
+export function saveIntoFnCache(key, code, params, ctx) {
+	if (ctx === NULL) {
+		return;
 	}
+
+	ctx['init'](Snakeskin);
+	if (!key || !(params.cache || $globalCache[key])) {
+		return;
+	}
+
+	$globalFnCache[key] = $C.extend(false, $globalFnCache[key], {[code]: ctx});
 }
 
 /**
  * Saves templates in the cache by the specified key
  *
- * @param {?string} cacheKey - cache key
+ * @param {?string} key - cache key
  * @param {string} code - source SS code
  * @param {!Object} params - compile parameters
- * @param {!Parser} parser - instance of Parser class
+ * @param {!Parser} parser - instance of a Parser class
  */
-export function saveIntoCache(cacheKey, code, params, parser) {
-	if (cacheKey && (params.cache || $globalCache[cacheKey])) {
-		$globalCache[cacheKey] = $globalCache[cacheKey] || {};
-		$globalCache[cacheKey][code] = {
-			debug: params.debug,
-			text: parser.res,
-			words: params.words
-		};
+export function saveIntoCache(key, code, params, parser) {
+	if (!key || !(params.cache || $globalCache[key])) {
+		return;
 	}
+
+	$globalCache[key] = $C.extend(false, $globalCache[key], {
+		debug: params.debug,
+		text: params.res,
+		words: params.words
+	});
 }
