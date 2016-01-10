de78bbc1b19421606661f888f51a5449cfa1bb6a
kobezzza <kobezzza@gmail.com>
2016-01-10 19:12:46 +0300

    :wrench:

--- src/directives/link.js
+++ src/directives/link.js
@@ -8,7 +8,6 @@
  * https://github.com/SnakeskinTpl/Snakeskin/blob/master/LICENSE
  */
 
-import $C from '../deps/collection';
 import Snakeskin from '../core';
 import { emptyCommandParams } from '../consts/regs';
 
