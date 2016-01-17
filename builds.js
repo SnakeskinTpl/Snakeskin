decdc6319de18726ce10d57810d08588a925c9d3
kobezzza <kobezzza@gmail.com>
2016-01-17 12:25:50 +0300

    :wrench:

--- builds.js
+++ builds.js
@@ -1,3 +1,13 @@
+'use strict';
+
+/*!
+ * Snakeskin
+ * https://github.com/SnakeskinTpl/Snakeskin
+ *
+ * Released under the MIT license
+ * https://github.com/SnakeskinTpl/Snakeskin/blob/master/LICENSE
+ */
+
 module.exports = {
 	'snakeskin': {
 		'compiler': true
