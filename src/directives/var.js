2e74d78b36570e628d6139be2c2db90905c8c599
kobezzza <kobezzza@gmail.com>
2016-01-17 18:41:46 +0300

    Added putIn integration, changed from inline to block, added shorthand / for classic mode

--- src/directives/var.js
+++ src/directives/var.js
@@ -14,13 +14,40 @@ Snakeskin.addDirective(
 	'var',
 
 	{
+		block: true,
+		deferInit: true,
 		group: 'var',
 		notEmpty: true,
 		shorthands: {':': 'var '}
 	},
 
 	function (command) {
-		this.append(this.declVars(command));
-	}
+		const
+			putIn = /^putIn\s+([^\s]+)$/.exec(command);
+
+		if (putIn) {
+			this.append(this.declVars(putIn[1]));
+			Snakeskin.Directives['putIn'].call(this, putIn[1]);
+
+		} else {
+			const
+				short = command.slice(-1) === '/';
+
+			if (short) {
+				command = command.slice(0, -1);
+			}
+
+			this.append(this.declVars(command));
+
+			if (short) {
+				this.startInlineDir();
+
+			} else {
+				this.startDir();
+			}
+		}
+	},
+
+	function () {}
 
 );
