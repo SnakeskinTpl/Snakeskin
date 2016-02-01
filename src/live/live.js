3d82477e694b65d0cfc71f40347afb1a5b265368
kobezzza <kobezzza@gmail.com>
2016-02-01 09:39:42 +0300

    Added new DOM API

--- src/live/live.js
+++ src/live/live.js
@@ -35,35 +35,175 @@ Snakeskin.StringBuffer = function () {
 };
 
 /**
+ * @param {!Function} child
+ * @param {!Function} parent
+ */
+function inherit(child, parent) {
+	const F = function () {
+		this.constructor = child;
+	};
+
+	F.prototype = parent.prototype;
+	child.prototype = new F();
+}
+
+/**
+ * Node constructor
+ * @constructor
+ */
+Snakeskin.Node = function () {};
+
+/**
+ * Returns the number of child elements
+ * @return {number}
+ */
+Snakeskin.Node.prototype.length = function () {
+	return this.value.childNodes.length;
+};
+
+/**
+ * Returns text content
+ * @return {number}
+ */
+Snakeskin.Node.prototype.textContent = function () {
+	return this.value.textContent;
+};
+
+/**
  * DocumentFragment constructor
  *
  * @constructor
- * @return {!DocumentFragment}
+ * @extends {Snakeskin.Node}
+ * @param {string} renderMode - rendering mode of templates
  */
-Snakeskin.DocumentFragment = function () {
-	return document.createDocumentFragment();
+Snakeskin.DocumentFragment = function (renderMode) {
+	this.renderMode = renderMode;
+	this.value = document.createDocumentFragment();
+};
+
+inherit(Snakeskin.DocumentFragment, Snakeskin.Node);
+
+/**
+ * Appends a child to the document fragment
+ * @param {?} el - element for appending
+ */
+Snakeskin.DocumentFragment.prototype.appendChild = function (el) {
+	this.value.appendChild(el);
 };
 
 /**
  * Element constructor
  *
  * @constructor
+ * @extends {Snakeskin.Node}
+ *
  * @param {string} name - element name
- * @return {!Element}
+ * @param {string} renderMode - rendering mode of templates
  */
-Snakeskin.Element = function (name) {
-	return document.createElement(name);
+Snakeskin.Element = function (name, renderMode) {
+	this.renderMode = renderMode;
+	this.value = document.createElement(name);
+};
+
+inherit(Snakeskin.Element, Snakeskin.Node);
+
+/**
+ * Appends a child to the element
+ * @param {?} el - element for appending
+ */
+Snakeskin.Element.prototype.appendChild = function (el) {
+	this.value.appendChild(el);
+};
+
+/**
+ * Sets an attribute to the element
+ *
+ * @param {string} name - attribute name
+ * @param {string} val - attribute value
+ */
+Snakeskin.Element.prototype.setAttribute = function (name, val) {
+	this.value.setAttribute(name, val);
 };
 
 /**
  * Comment constructor
  *
  * @constructor
+ * @extends {Snakeskin.Node}
+ *
  * @param {string} text - comment text
- * @return {!Comment}
+ * @param {string} renderMode - rendering mode of templates
  */
-Snakeskin.Comment = function (text) {
-	return document.createComment(text);
+Snakeskin.Comment = function (text, renderMode) {
+	this.renderMode = renderMode;
+	this.value = document.createComment(text);
+};
+
+inherit(Snakeskin.Comment, Snakeskin.Node);
+
+/**
+ * Text constructor
+ *
+ * @constructor
+ * @extends {Snakeskin.Node}
+ *
+ * @param {string} text
+ * @param {string} renderMode - rendering mode of templates
+ */
+Snakeskin.Text = function (text, renderMode) {
+	this.renderMode = renderMode;
+	this.value = document.createTextNode(text);
+};
+
+inherit(Snakeskin.Text, Snakeskin.Node);
+
+/**
+ * Map of inline tag names
+ * @const
+ */
+Snakeskin.inlineTags = {
+	'area': 'href',
+	'base': 'href',
+	'br': true,
+	'col': true,
+	'embed': 'src',
+	'hr': true,
+	'img': 'src',
+	'input': 'value',
+	'link': 'href',
+	'meta': 'content',
+	'param': 'value',
+	'source': 'src',
+	'track': 'src',
+	'wbr': true
+};
+
+/**
+ * Appends a value to the specified element
+ *
+ * @param {(!Snakeskin.DocumentFragment|!Snakeskin.Element)} el - base element
+ * @param {?} val - value for appending
+ * @param {string} renderMode - rendering mode of templates
+ * @return {(!Snakeskin.Element|!Snakeskin.Comment|!Snakeskin.Text)}
+ */
+Snakeskin.appendChild = function (el, val, renderMode) {
+	if (val instanceof Snakeskin.Node === false) {
+		val = Snakeskin.Text(String(val), renderMode);
+	}
+
+	el.appendChild(val);
+	return val;
+};
+
+/**
+ * Sets an attribute to the specified element
+ *
+ * @param {!Snakeskin.Node} node - source element
+ * @param {string} name - attribute name
+ * @param {?} val - attribute value
+ */
+Snakeskin.setAttribute = function (node, name, val) {
+	node.setAttribute(name, val instanceof Snakeskin.Node ? val.textContent : String(val));
 };
 
 const
@@ -161,54 +301,6 @@ Snakeskin.forIn = function (obj, callback) {
 
 		i++;
 	}
-};
-
-/**
- * Map of empty tag names
- * @const
- */
-Snakeskin.inlineTags = {
-	'area': 'href',
-	'base': 'href',
-	'br': true,
-	'col': true,
-	'embed': 'src',
-	'hr': true,
-	'img': 'src',
-	'input': 'value',
-	'link': 'href',
-	'meta': 'content',
-	'param': 'value',
-	'source': 'src',
-	'track': 'src',
-	'wbr': true
-};
-
-/**
- * Appends a node or a text to the source
- *
- * @param {!Element} node - source element
- * @param {?} obj - element for appending
- * @return {(!Element|!Text)}
- */
-Snakeskin.appendChild = function (node, obj) {
-	if (obj instanceof Node === false) {
-		obj = document.createTextNode(String(obj));
-	}
-
-	node.appendChild(any(obj));
-	return obj;
-};
-
-/**
- * Sets an attribute to the specified element
- *
- * @param {!Element} node - source element
- * @param {string} name - attribute name
- * @param {?} val - attribute value
- */
-Snakeskin.setAttribute = function (node, name, val) {
-	node.setAttribute(name, val instanceof Node === false ? String(val) : val.textContent);
 };
 
 /**
