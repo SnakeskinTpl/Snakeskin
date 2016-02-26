90ad8967968ff1499ef4af1838ff18f142a84626
kobezzza <kobezzza@gmail.com>
2016-02-26 10:49:33 +0300

    https://github.com/SnakeskinTpl/Snakeskin/issues/58

--- src/directives/yield.js
+++ src/directives/yield.js
@@ -16,19 +16,56 @@ Snakeskin.addDirective(
 
 	{
 		ancestorsBlacklist: Snakeskin.group('function'),
+		block: true,
+		deferInit: true,
 		generator: true,
 		group: 'yield',
 		placement: 'template'
 	},
 
 	function (command) {
-		if (command) {
-			this.append(`yield ${this.out(command, {unsafe: true})};`);
+		if (command.slice(-1) === '/') {
+			this.startInlineDir(null, {command: command.slice(0, -1), short: true});
+			return;
+		}
 
-		} else {
+		this.startDir(null, {command});
+
+		if (!command) {
+			this.append(ws`
+				${this.declVars('__CALL_CACHE__ = __RESULT__', {sys: true})}
+				__RESULT__ = ${this.getResultDecl()};
+			`);
+		}
+	},
+
+	function () {
+		const
+			p = this.structure.params;
+
+		if (p.command) {
+			this.append(`yield ${this.out(p.command, {unsafe: true})};`);
+
+		} else if (p.short) {
 			this.append(ws`
 				yield ${this.getReturnResultDecl()};
 				__RESULT__ = ${this.getResultDecl()};
+			`);
+
+		} else {
+			const
+				tmp = this.getVar('__CALL_CACHE__');
+
+			this.append(ws`
+				if (__LENGTH__(__RESULT__)) {
+					yield ${this.getReturnResultDecl()};
+					__RESULT__ = ${tmp};
+
+				} else {
+					__RESULT__ = ${tmp};
+					yield ${this.getReturnResultDecl()};
+					__RESULT__ = ${this.getResultDecl()};
+				}
 			`);
 		}
 	}
