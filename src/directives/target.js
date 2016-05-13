43afe66b337875b36099ed2e13440791f347925d
kobezzza <kobezzza@gmail.com>
2016-05-13 12:27:37 +0300

    Added the short end for classic mode

--- src/directives/target.js
+++ src/directives/target.js
@@ -25,13 +25,20 @@ Snakeskin.addDirective(
 
 	function (command) {
 		const
+			short = command.slice(-1) === '/';
+
+		if (short) {
+			command = command.slice(0, -1);
+		}
+
+		const
 			[obj, ref] = command.split(/\s+as\s+/);
 
 		if (ref) {
 			this.declVar(ref);
 		}
 
-		this.startDir();
+		this.startDir(null, {short});
 		let str = this.declVars(`__TARGET_REF__ = ${obj}`, {sys: true});
 		this.structure.params.ref = this.getVar('__TARGET_REF__');
 
@@ -39,43 +46,53 @@ Snakeskin.addDirective(
 			str += this.out(`var ${ref} = __TARGET_REF__;`, {skipFirstWord: true, unsafe: true});
 		}
 
-		this.append(ws`
-			${str}
-
-			${this.declVars(
-				ws`
-					__CALL_CACHE__ = __RESULT__,
-					__CALL_TMP__ = [],
-					__CALL_POS__ = 0
-				`,
-				{sys: true}
-			)}
-
-			__RESULT__ = ${this.getResultDecl()};
-		`);
-	},
-
-	function () {
-		const
-			p = this.structure.params;
-
-		if (p.strongSpace) {
-			this.strongSpace.pop();
-		}
-
-		this.append(`__RESULT__ = __TARGET_END__(__RESULT__, ${this.getVar('__CALL_TMP__')}, ${p.ref});`);
-
-		const
-			parent = any(this.hasParentMicroTemplate());
-
-		if (parent) {
-			this.append(`__RESULT__ = new Raw(${p.ref});`);
-			parent.params.strongSpace = true;
-			this.strongSpace.push(true);
+		if (short) {
+			this.append(str);
+			end.call(this);
+			this.endDir();
 
 		} else {
-			this.append(`__RESULT__ = ${this.getVar('__CALL_CACHE__')};`);
+			this.append(ws`
+				${str}
+	
+				${this.declVars(
+					ws`
+							__CALL_CACHE__ = __RESULT__,
+							__CALL_TMP__ = [],
+							__CALL_POS__ = 0
+						`,
+					{sys: true}
+				)}
+		
+					__RESULT__ = ${this.getResultDecl()};
+			`);
 		}
+	},
+
+	end
+);
+
+function end() {
+	const
+		p = this.structure.params;
+
+	if (p.strongSpace) {
+		this.strongSpace.pop();
 	}
 
-);
+	if (!p.short) {
+		this.append(`__RESULT__ = __TARGET_END__(__RESULT__, ${this.getVar('__CALL_TMP__')}, ${p.ref});`);
+	}
+
+	const
+		parent = any(this.hasParentMicroTemplate());
+
+	if (parent) {
+		this.append(`__RESULT__ = new Raw(${p.ref});`);
+		parent.params.strongSpace = true;
+		this.strongSpace.push(true);
+
+	} else if (!p.short) {
+		this.append(`__RESULT__ = ${this.getVar('__CALL_CACHE__')};`);
+	}
+}
