01fd2645b08189568b0fed25652b0f8b1d37e9d5
kobezzza <kobezzza@gmail.com>
2018-01-16 18:06:08 +0300

    :arrow_up:

--- dist/snakeskin.live.js
+++ dist/snakeskin.live.js
@@ -5,7 +5,7 @@
  * Released under the MIT license
  * https://github.com/SnakeskinTpl/Snakeskin/blob/master/LICENSE
  *
- * Date: 'Tue, 20 Jun 2017 16:12:31 GMT
+ * Date: 'Tue, 16 Jan 2018 15:03:43 GMT
  */
 
 (function (global, factory) {
@@ -77,7 +77,6 @@ String.prototype.trim = String.prototype.trim || function () {
  * @param {?} obj - source value
  * @return {boolean}
  */
-
 function isFunction(obj) {
   return typeof obj === 'function';
 }
@@ -471,7 +470,6 @@ Snakeskin$1.decorate = function (decorators, fn) {
  * @param {?} val - source object
  * @return {?}
  */
-
 function any(val) {
   return val;
 }
@@ -485,7 +483,7 @@ function any(val) {
  */
 
 
-var rRgxp = /([\\\/'*+?|()\[\]{}.^$-])/g;
+var rRgxp = /([\\/'*+?|()[\]{}.^$-])/g;
 
 /**
  * Escapes RegExp characters in a string
@@ -535,7 +533,118 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 
 
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
 
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
 
 
 
